fd7e93fa1e473ace31678912ed83b21283122d37
Bimo Aji Widyantoro <235257829+widyantoro-id@users.noreply.github.com>
2026-04-08 10:06:06 +0800

    Create 01_cek_kesediaan_citra_satelit.js
    membuat script awal

---
+++ 01_pengecekan_citra_satelit.js
@@ -0,0 +1,11 @@
+var lokasi = geometry
+
+var citraSatelit = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2")
+    
+    .filterBounds(lokasi)
+    
+    .filterDate('2023-05-01', '2023-10-31')
+    
+    .filterMetadata('CLOUD_COVER', 'less_than', 30);
+
+print(citraSatelit);

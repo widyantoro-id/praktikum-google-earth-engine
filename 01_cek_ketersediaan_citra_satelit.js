e1b978a46db40251069b2143d697c065476bdc27
Bimo Aji Widyantoro <235257829+widyantoro-id@users.noreply.github.com>
2026-04-08 10:09:39 +0800

    penambahan comment

--- 01_cek_ketersediaan_citra_satelit.js
+++ 01_cek_ketersediaan_citra_satelit.js
@@ -8,4 +8,5 @@ var citraSatelit = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2")
     
     .filterMetadata('CLOUD_COVER', 'less_than', 30);
 
+// menampilkan informasi hasil dari citra satelit yang difilter
 print(citraSatelit);

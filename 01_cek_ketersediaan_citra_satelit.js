4341ad35dccbc1a95b7687f56e731e6747f35ed7
Bimo Aji Widyantoro <235257829+widyantoro-id@users.noreply.github.com>
2026-04-08 10:11:07 +0800

    Update.js

--- 01_cek_ketersediaan_citra_satelit.js
+++ 01_cek_ketersediaan_citra_satelit.js
@@ -1,3 +1,16 @@
+var geometry = 
+    /* color: #d63000 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[116.69331054687498, -0.9141536811985339],
+          [116.69331054687498, -1.4029438566092867],
+          [117.26459960937498, -1.4029438566092867],
+          [117.26459960937498, -0.9141536811985339]]], null, false);
+
 var lokasi = geometry
 
 var citraSatelit = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2")
